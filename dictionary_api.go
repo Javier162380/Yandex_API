49eecb0df630837ef857a3a4f4508642fc331099
Javier162380 <javierllorente16@gmail.com>
2018-11-10 11:54:26 +0100

    adding second draft of yandex api library

---
+++ dictionary_api.go
@@ -0,0 +1,10 @@
+package yandexapi
+
+type Lookup {
+	Head map[string]string `json:"head"`
+	Dictionary Definition
+}
+
+type Definition {
+	
+}
--- example/example.go
+++ example/example.go
@@ -7,10 +7,17 @@ import (
 )
 
 func main() {
-	yandex_api_key := os.Args[1]
+	yandex_api_key := os.Args[1] //<<INSERT YOUR YANDEX API KEY HERE>>
 	fmt.Printf("%s", yandex_api_key)
-	language_translation := yandexapi.Get_yandex_languages(
-		yandex_api_key,
+	languagetranslation := yandexapi.Getyandexlanguages(yandex_api_key, "en")
+	fmt.Printf("%s", languagetranslation.Language)
+
+	detectlanguage := yandexapi.Detectlanguage(yandex_api_key, "españa")
+	fmt.Printf("%s", detectlanguage.DecodeLanguage)
+
+	translatemessage := yandexapi.Gettexttranslation(yandex_api_key,
+		"estoy temblando",
 		"en")
-	fmt.Printf("%s", language_translation.Translation)
+	fmt.Printf("%s", translatemessage.Translatetext)
+
 }
